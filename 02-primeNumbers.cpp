40332c16ea4a48c802b1291cd8fea57ca16945b0
Serene Liu <serene6197@gmail.com>
2022-08-08 15:25:32 +0100

    Alter solution to 02 slightly more efficient

--- 02-primeNumbers.cpp
+++ 02-primeNumbers.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 using namespace std;
 
 // THIS METHOD FOR FINDING PRIMES IS TOO INEFFICIENT 
-bool isPrime(int n){
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++){
-        if (n % i == 0) return false;
+// bool isPrime(int n){
+//     // Check from 2 to n-1
+//     for (int i = 2; i < n; i++){
+//         if (n % i == 0) return false;
+//     }
+//     return true;
+// }
+
+// BRUTE FORCE METHOD FOR VERIFYING IF PRIME
+bool isPrime(long int num){
+    if (num <= 1) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false;
+    else{
+        bool prime = true;
+        int divisor = 3;
+        int upperLimit = sqrt(num) + 1;
+
+        while (divisor <= upperLimit){
+            if (num % divisor == 0) return false;
+            divisor += 2;
+        }
+        return prime;
     }
-    return true;
 }
+
+//SEGMENTED SIEVE OF ERATOSTHENES?
 
 int main(){
     int testCases;
@@ -24,4 +46,5 @@ int main(){
         }
 
     }
+    return 0;
 }
